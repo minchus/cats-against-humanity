d08e581e8f5f4dbd9b03340dab3f8dcc67f2eeda
Mingu <mcchung2718@users.noreply.github.com>
2020-04-18 20:19:38 +0100

    start designing

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Enter Room ID"
+                    name="room-id"
+                    type="text"
+                  />
+
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="success">Create game</v-btn>
+                <v-spacer />
+                <v-btn color="primary">Join</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'stuff',
+  props: {
+    source: String
+  }
+}
+</script>
